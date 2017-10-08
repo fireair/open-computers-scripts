39bed18bc3266f5f0a505c0ecc65186c9820db2b
fireair <31391039+fireair@users.noreply.github.com>
2017-10-08 15:56:55 -0500

    Create level1.lua

---
+++ level1.lua
@@ -0,0 +1,13 @@
+local component = require("component")
+local rs = component.redstone
+
+
+rs.setWirelessFrequency(1)
+rs.setWirelessOutput(true)
+rs.setWirelessOutput(false)
+rs.setWirelessFrequency(2)
+rs.setWirelessOutput(true)
+rs.setWirelessOutput(false)
+rs.setWirelessFrequency(10)
+rs.setWirelessOutput(true)
+rs.setWirelessOutput(false)
